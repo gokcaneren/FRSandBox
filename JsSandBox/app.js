20f68cc03dafdbe9ca4e4cbe41c18561a813ff5a
gokcaneren <gokcann01@gmail.com>
2022-11-07 22:04:52 +0300

    Using console

--- JsSandBox/app.js
+++ JsSandBox/app.js
@@ -1,1 +1,25 @@
-alert('Hello world!');
+console.log('HHelllloooooooooooo12312');
+
+console.log(123);
+
+console.log(false);
+
+console.log([1,2,3,4]);
+
+console.log({a:1, b:2, c:'sawe'});
+
+console.table({a:1, b:2, c:'sawe'});
+
+console.error('This is an error!');
+
+console.warn("Warning!");
+
+console.time('Start!');
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    console.log(6);
+    console.log(7);
+console.timeEnd('Finish!');
